cd07ee1893aca4b5e2619b21c570d63315c3ae5d
23641a0418-prog <23641a0418@vaagdevi.edu.in>
2026-06-29 01:05:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import ProductDetail from './pages/ProductDetail'; // Imported the new showcase page
-import Process from './pages/Process';
-import OurStory from './pages/OurStory';
-import CustomDesign from './pages/CustomDesign';
-import AdminDashboard from './pages/AdminDashboard';
-import Cart from './pages/Cart';
-import { CartProvider } from './context/CartContext';
-
-function LayoutWrapper() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
-
-  return (
-    <div className="min-h-screen bg-tassar-cream text-tassar-earth selection:bg-tassar-raw selection:text-tassar-earth">
-      {!isAdminRoute && <Navbar />}
-
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/product/:id" element={<ProductDetail />} /> {/* Dynamic Route Added */}
-          <Route path="/process" element={<Process />} />
-          <Route path="/story" element={<OurStory />} />
-          <Route path="/custom-design" element={<CustomDesign />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </AnimatePresence>
-    </div>
-  );
-}
+import Home from './Home'; // Or './pages/Home' depending on where you put it
 
 export default function App() {
-  return (
-    <CartProvider>
-      <Router>
-        <LayoutWrapper />
-      </Router>
-    </CartProvider>
-  );
+  return <Home />;
 }
